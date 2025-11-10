7a6cd433f1926747de93d32f2df6ab58cafde564
Bimpyton <luke.j.barrows@gmail.com>
2025-11-10 16:47:13 +0200

    Sound Added

--- Procedural_TowerDefence_Unity/Assets/Scripts/VolumeSettings.cs
+++ Procedural_TowerDefence_Unity/Assets/Scripts/VolumeSettings.cs
@@ -8,4 +8,20 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private Slider masterSlider;
     [SerializeField] private Slider musicSlider;
     [SerializeField] private Slider sfxSlider;
+
+
+    public void SetMasterVolume(float volume)
+    {
+        audioMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+    }
 }
